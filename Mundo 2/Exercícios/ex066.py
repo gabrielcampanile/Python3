fc24ab17d280752dfa4256bdcc8cb06a43b2c9a0
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2024-06-02 13:54:23 -0300

    Exercícios

---
+++ Mundo 2/Exercícios/ex066.py
@@ -0,0 +1,13 @@
+'''Crie um programa que leia números inteiros pelo teclado. 
+O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. 
+No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).'''
+
+cont = s = 0
+
+while True:
+    n = int(input('Digite um número [999 para parar]: '))
+    if n == 999:
+        break
+    cont += 1
+    s += n
+print(f'A soma dos {cont} valores foi {s}!')
