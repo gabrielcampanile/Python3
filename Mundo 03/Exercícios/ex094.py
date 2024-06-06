836a05cc227bcc5eccc2278cb03e966b0e2ffd79
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2024-06-05 22:28:41 -0300

    Exercícios

--- Mundo 03/Exercícios/ex094.py
+++ Mundo 03/Exercícios/ex094.py
@@ -11,31 +11,41 @@ media = 0
 
 while True:
     pessoa['nome'] = str(input('Nome: ')).strip().title()
-    pessoa['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
     pessoa['idade'] = int(input('Idade: '))
     media += pessoa['idade']
     grupo.append(pessoa.copy())
 
-    resp = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
-    while resp not in 'sn':
-        print('ERRO! Responda apenas S ou N.')
+    while True:
         resp = str(input('Quer continuar? [S/N] ')).strip().lower()[0]
-    if resp in 'n':
+        if resp in 'sn':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'n':
         break
 
 print('-=' * 30)
-print(f'- O grupo tem {len(grupo)} pessoas.')
+print(f'A) O grupo tem {len(grupo)} pessoas.')
 
 media /= len(grupo)
-print(f'- A média de idade é de {media:.2f} anos.')
+print(f'B) A média de idade é de {media:.2f} anos.')
 
-print(f'- As mulheres cadastradas foram: ', end='')
+print(f'C) As mulheres cadastradas foram: ', end='')
 for p in grupo:
     if p['sexo'] == 'F':
         print(f'{p["nome"]}', end=' ')
 print()
 
-print(f'- Lista das pessoas que estão acima da média: ')
+print(f'D) Lista das pessoas que estão acima da média: ')
 for p in grupo:
     if p['idade'] > media:
-        print(f'    Nome = {p["nome"]}; Sexo = {p["sexo"]}; Idade = {p["idade"]}')
+        # print(f'    Nome = {p["nome"]}; Sexo = {p["sexo"]}; Idade = {p["idade"]}')
+        print('    ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<<< ENCERRADO >>>')
