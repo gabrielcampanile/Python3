5f2e807e094d2c82752e709d28e6129aa52b648a
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2024-06-07 19:03:17 -0300

    Exercícios

---
+++ Mundo 03/Aulas/Aula22-Modularização e Pacotes/Modularização01.py
@@ -0,0 +1,7 @@
+import Uteis
+
+# Programa Principal
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é {Uteis.fatorial(n)}')
+print(f'O dobro de {n} é {Uteis.dobro(n)}')
+print(f'O triplo de {n} é {Uteis.triplo(n)}')
---
+++ Mundo 03/Aulas/Aula22-Modularização e Pacotes/Modularização02.py
@@ -0,0 +1,7 @@
+from Pacotes import numeros
+
+# Programa Principal
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é {numeros.fatorial(n)}')
+print(f'O dobro de {n} é {numeros.dobro(n)}')
+print(f'O triplo de {n} é {numeros.triplo(n)}')
